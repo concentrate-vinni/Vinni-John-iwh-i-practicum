b9bf31bc24ea95dad0955cbcd029b3b82c65482d
bactran-chch <158534203+bactran-chch@users.noreply.github.com>
2025-04-08 14:06:25 +1200

    add keys

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-ap1-24c775bd-69f2-4515-948d-d3bd24899930';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
